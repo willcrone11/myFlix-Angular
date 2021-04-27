bab96d68828864ce30ebf172072b20839e200afd
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-04-27 15:28:39 -0400

    Adds logic for displaying director and genre view dialogs

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { GetAllMoviesService } from '../fetch-api-data.service'
+import { GetAllMoviesService, AddFavoriteMovieService } from '../fetch-api-data.service';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
+
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -8,7 +14,12 @@ import { GetAllMoviesService } from '../fetch-api-data.service'
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: GetAllMoviesService) { }
+  constructor(
+    public fetchApiData: GetAllMoviesService,
+    public fetchApiData2: AddFavoriteMovieService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    ) { }
 
 ngOnInit(): void {
   this.getMovies();
@@ -19,6 +30,45 @@ getMovies(): void {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  showDirectorDialog(
+    name: string,
+    bio: string,
+    birth: Date,
+    death: Date
+  ): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: { name, bio, birth, death },
+      width: '350px',
+    });
+  }
+
+  showGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreViewComponent, {
+      data: { name, description },
+      width: '350px',
+    });
+  }
+  showDetailsDialog(
+    title: string,
+    imagepath: string,
+    description: string,
+    director: string,
+    genre: string
+  ): void {
+    this.dialog.open(MovieDetailsComponent, {
+      data: { title, imagepath, description, director, genre },
+      width: '350px',
+    });
+  }
+
+  addFavorite(id: string, title: string): void {
+    this.fetchApiData2.addFavoriteMovie(id).subscribe(() => {
+      this.snackBar.open(`${title} has been added to your favorites!`, 'OK', {
+        duration: 2000,
+      });
     });
   }
 
